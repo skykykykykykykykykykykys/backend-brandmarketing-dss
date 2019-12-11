73a31579d9067e72a8fe001ccc778e8615caaa3a
Ismail F Aziz <25928284+skykykykykykykykykykykys@users.noreply.github.com>
2019-12-11 17:37:24 +0700

    provide png export method

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 from flask import Flask, render_template, request
+import jinja2
 import requests
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 app = Flask(__name__)
+my_loader = jinja2.ChoiceLoader([
+     app.jinja_loader,
+     jinja2.FileSystemLoader('/templates')
+])
+app.jinja_loader = my_loader
 
-@app.route('/')
-def show_tables():
+@app.route('/asia')
+def show_visual():
      data = pd.read_csv('/data/data.csv')
      #Provide Pie Chart with 
+
+     fig, ax = plt.subplots()
 
      country_data = data["contingent"]
      medal_data = data["gold_medal"]
@@ -17,18 +26,21 @@ def show_tables():
      explode = (0.1, 0, 0, 0, 0)  
      plt.pie(medal_data, labels=country_data, explode=explode, colors=colors,
      autopct='%1.1f%%', shadow=True, startangle=140)
-     plt.title("Gold medal achievements of five most successful\n"+"contingents in 2016 Summer Olympics")
+     plt.title("Channel Grouping from Asia Continents")
      plt.show()
 
 
 
-     
-     data.set_index(['Name'], inplace=True)
-     data.index.name=None
-     females = data.loc[data.Gender=='f']
-     males = data.loc[data.Gender=='m']
-     return render_template('view.html',tables=[females.to_html(classes='female'), males.to_html(classes='male')],
-     titles = ['na', 'Female surfers', 'Male surfers'])
+     #exporting to png
+     canvas = FigureCanvas(fig)
+     img = BytesIO()
+     fig.savefig(img)
+     img.seek(0)
+     return send_file(img, mimetype='image/png')
+
+@app.route('/')
+def index():
+     return render_template('index.html')
 
 if __name__ == '__main__':
      app.run(debug=True)
